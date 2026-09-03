44a09b2141d04e99ac7b4a370cc3852aa3c1a7c7
Jayraj <jaybhatta@yahoo.com>
2026-09-03 20:49:25 +0545

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/portfolio/',
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
